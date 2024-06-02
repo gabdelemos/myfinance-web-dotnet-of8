3b2d9d06fe4298620b4b07d8df491b7707375d2f
Gabriella Lemos <gab.delemos@gmail.com>
2024-06-02 17:15:15 -0300

    add new files

--- myfinance-web-netcore/obj/Debug/net8.0/myfinance-web-netcore.AssemblyInfo.cs
+++ myfinance-web-netcore/obj/Debug/net8.0/myfinance-web-netcore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myfinance-web-netcore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d8fd5bc7efdd2031458ef93a4edaae7c8df835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ad8f681a6a3e1cbc7ab766b53eac39f6e9b435")]
 [assembly: System.Reflection.AssemblyProductAttribute("myfinance-web-netcore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myfinance-web-netcore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
