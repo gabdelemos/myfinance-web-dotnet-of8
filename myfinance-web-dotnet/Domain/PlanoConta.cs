3df9d6a0fd7ffd3b0e53b6de49d711b7454e9b24
Gabriella Lemos <gab.delemos@gmail.com>
2025-05-26 14:52:49 -0300

    subindo commit

--- myfinance-web-dotnet/Domain/PlanoConta.cs
+++
@@ -1,9 +0,0 @@
-namespace myfinance_web_netcore.Domain
-{
-    public class PlanoConta
-    {
-        public int Id { get; set; }
-        public required string Descricao { get; set; }
-        public required string Tipo { get; set; }
-    }
-}
